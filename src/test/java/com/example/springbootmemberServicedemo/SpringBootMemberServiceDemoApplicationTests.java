664be14e3cec196e2b7a54737f8897d720afa134
LEEJ0NGWAN <lastffx@gmail.com>
2020-10-14 20:16:43 +0900

    프로젝트 초기 생성

---
+++ src/test/java/com/example/springbootmemberServicedemo/SpringBootMemberServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmemberServicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMemberServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
