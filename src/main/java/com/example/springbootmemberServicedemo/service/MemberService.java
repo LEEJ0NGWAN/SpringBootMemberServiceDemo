64e6b395f7b4c6d646c8533ab0eb6671d7330e20
LEEJ0NGWAN <lastffx@gmail.com>
2020-10-15 21:44:31 +0900

    회원 서비스

---
+++ src/main/java/com/example/springbootmemberServicedemo/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.example.springbootmemberServicedemo.service;
+
+import com.example.springbootmemberServicedemo.domain.Member;
+import com.example.springbootmemberServicedemo.repository.MemberRepository;
+import com.example.springbootmemberServicedemo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복 검사
+
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 특정 회원 조회
+    public Optional<Member> findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
