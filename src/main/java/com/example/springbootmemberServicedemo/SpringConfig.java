f487126d43fac9bcdd498c6a40388652c33a136e
LEEJ0NGWAN <lastffx@gmail.com>
2020-10-28 00:52:21 +0900

    멤버 리포지토리 h2 데이터베이스로 교체

--- src/main/java/com/example/springbootmemberServicedemo/SpringConfig.java
+++ src/main/java/com/example/springbootmemberServicedemo/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.example.springbootmemberServicedemo;
 
+import com.example.springbootmemberServicedemo.repository.JdbcMemberRepository;
 import com.example.springbootmemberServicedemo.repository.MemberRepository;
 import com.example.springbootmemberServicedemo.repository.MemoryMemberRepository;
 import com.example.springbootmemberServicedemo.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +25,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
 
